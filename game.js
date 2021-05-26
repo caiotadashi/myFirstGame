ba6ef16a0d45ed4c211ff3b5e20b277daff83c3c
Caio Tadashi <ctdsh@icloud.com>
2021-05-26 17:34:22 -0300

    improved design pattern

--- game.js
+++ game.js
@@ -2,37 +2,83 @@ const screen = document.getElementById('screen');
 const context = screen.getContext('2d');
 const currentPlayerId = 'player1';
 
-const game = {
-    players: {
-        'player1': { x: 1, y: 2 },
-        'player2': { x: 5, y: 8 }
-    },
-    fruits: {
-        'fruit1': { x: 0, y: 1 }
+function createGame() {
+    const state = {
+        players: {
+            'player1': { x: 1, y: 2 },
+            'player2': { x: 5, y: 8 }
+        },
+        fruits: {
+            'fruit1': { x: 0, y: 1 }
+        }
+    };
+
+    function movePlayer(command) {
+        console.log(`Moving ${command.playerId} with ${command.keyPressed}`);
+
+        const keyPressed = command.keyPressed;
+        const player = state.players[command.playerId];
+
+        if (keyPressed === 'ArrowUp' && player.y - 1 >= 0){
+            player.y -= 1;
+            return
+        }
+        if (keyPressed === 'ArrowRight' && player.x + 1 < screen.width){
+            player.x += 1;
+            return
+        }
+        if (keyPressed === 'ArrowDown' && player.y + 1 < screen.height){
+            player.y += 1;
+            return
+        }
+        if (keyPressed === 'ArrowLeft' && player.x - 1 >= 0){
+            player.x -= 1;
+            return
+        }
     }
+
+    return {
+        movePlayer,
+        state
+    };
 };
 
-document.addEventListener('keydown', handleKeyDown);
+const game = createGame();
+const keyboardListener = createKeyboardListener();
+keyboardListener.subscribe(game.movePlayer);
 
-function handleKeyDown(event) {
-    const keyPressed = event.key;
-    const player = game.players[currentPlayerId]
+function createKeyboardListener() {
+    const state ={
+        observers: []
+    }
 
-    if (keyPressed == 'ArrowUp' && player.y - 1 >= 0){
-        player.y -= 1;
-        return;
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction);
     }
-    if (keyPressed == 'ArrowRight' && player.x + 1 < screen.width){
-        player.x += 1;
-        return;
+
+    function notifyAll(command) {
+        console.log(`Notifyng ${state.observers.length} observers`);
+
+        for (const observerFunction of state.observers) {
+            observerFunction(command);
+        }
     }
-    if (keyPressed == 'ArrowDown' && player.y + 1 < screen.height){
-        player.y += 1;
-        return;
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    function handleKeyDown(event) {
+        const keyPressed = event.key;
+
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        }
+
+        notifyAll(command)
     }
-    if (keyPressed == 'ArrowLeft' && player.x - 1 >= 0){
-        player.x -= 1;
-        return;
+
+    return {
+        subscribe
     }
 }
 
@@ -42,14 +88,14 @@ function renderScreen(){
     context.fillStyle = 'white';
     context.clearRect(0,0,10,10);
 
-    for (const playerId in game.players) {
-        const player = game.players[playerId];
+    for (const playerId in game.state.players) {
+        const player = game.state.players[playerId];
         context.fillStyle = 'black';
         context.fillRect(player.x, player.y, 1, 1);
     }
 
-    for (const fruitId in game.fruits) {
-        const fruit = game.fruits[fruitId];
+    for (const fruitId in game.state.fruits) {
+        const fruit = game.state.fruits[fruitId];
         context.fillStyle = 'green';
         context.fillRect(fruit.x, fruit.y, 1, 1);
     }
